1e93ce0e731cf4a885c93cdcc074fa21490b2fc3
Francisco G <fsalvador23@gmail.com>
2019-08-30 11:34:08 +0200

    Updated charts

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
-import '/imports/ui/App.css';;
+import '/imports/ui/App.css';
 import Barchart    from '/imports/ui/barchart/barchart.js'
+import Radarchart  from '/imports/ui/radarchart/radarchart.js'
+import Scatterplot from '/imports/ui/scatterplot/scatterplot.js'
+import Piechart    from '/imports/ui/piechart/piechart.js'
+import Timeseries  from '/imports/ui/timeseries/timeseries.js'
+
+import _ from 'lodash'
 /* Import React Components */
 import { Climate } from '/imports/api/tasks.js';
 
@@ -12,33 +18,137 @@ class App extends Component {
   }
 
   render() {
+    console.log(this.props.MeanTemperature);
+    //console.log(this.props.Wind);
     const BarData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }]
+    };
+    const RadarData = {
+      labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: 'rgba(179,181,198,0.2)',
+          borderColor: 'rgba(179,181,198,1)',
+          pointBackgroundColor: 'rgba(179,181,198,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(179,181,198,1)',
+          data: [65, 59, 90, 81, 56, 55, 40]
+        },
+        {
+          label: 'My Second dataset',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          pointBackgroundColor: 'rgba(255,99,132,1)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgba(255,99,132,1)',
+          data: [28, 48, 40, 19, 96, 27, 100]
+        }
+      ]
+    };
+
+    const ScatterData = {
+      labels: ['Scatter'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          fill: false,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [
+            { x: 65, y: 75 },
+            { x: 59, y: 49 },
+            { x: 80, y: 90 },
+            { x: 81, y: 29 },
+            { x: 56, y: 36 },
+            { x: 55, y: 25 },
+            { x: 40, y: 18 },
+          ]
+        }
+      ]
+    };
+    const PieData = {
+      labels: [
+        'Red',
+        'Green',
+        'Yellow'
+      ],
+      datasets: [{
+        data: [300, 50, 100],
+        backgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56'
+        ]
+      }]
+    };
+
+    const TimeData = {
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
       datasets: [
         {
           label: 'My First dataset',
-          backgroundColor: 'rgba(255,99,132,0.2)',
-          borderColor: 'rgba(255,99,132,1)',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
           data: [65, 59, 80, 81, 56, 55, 40]
         }
       ]
     };
     return(
-    <div className="container">
-    <Barchart data={BarData}></Barchart>
-    <h1>Wind:</h1>
-    <h1>Temperature:</h1>
-    <h1>Humidity:</h1>
-    <h1>Soil:</h1>
-    </div>);
-  } // end of render
-} // end of class
+      <div className="main-container">
+        <Barchart    title="Wind"        data={BarData}     />
+        <Radarchart  title="Temperature" data={RadarData}   />
+        <Scatterplot title="Humidity"    data={ScatterData} />
+        <Piechart    title="Soil"        data={PieData}     />
+        <Timeseries  title="Something"   data={TimeData}    />
+      </div>);
+    } // end of render
+  } // end of class
 
-export default withTracker((props) => {
-  return {
-    Climate: Climate.find({}).fetch()
-  };
-})(App);
+  export default withTracker((props) => {
+    return {
+      Wind:        Climate.find({}).fetch(),
+      MeanTemperature: _.map(Climate.find({}).fetch(), "S"),
+      Humidity:    Climate.find({}).fetch(),
+      Soil:        Climate.find({}).fetch(),
+    };
+  })(App);
