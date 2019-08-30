7de4b11ae77874a8c43231f43b4d4375d827efe0
Francisco G <fsalvador23@gmail.com>
2019-08-30 10:50:03 +0200

    Updated Climate

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,57 +1,31 @@
 import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
-import '/imports/ui/App.css';
-
+import '/imports/ui/App.css';;
+import Barchart    from '/imports/ui/barchart/barchart.js'
+/* Import React Components */
 import { Climate } from '/imports/api/tasks.js';
 
-import {Bar} from 'react-chartjs-2';
-
-
-Chart.defaults.global.responsive = true;
-
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 40]
-    }
-  ]
-};
-
-// App component - represents the whole app
 class App extends Component {
+
   constructor(props) {
     super(props);
   }
 
   render() {
-    const chartData = {
-      labels: [],
-      datasets: []
-    };
-    return(<div className="myChart">
-    <Bar
-    data={data}
-    width={100}
-    height={50}
-    options={{
-      maintainAspectRatio: false
-    }}
-    />
-    Wind:
-    Temperature:
-    Humidity:
-    Soil:
+    console.log(this.props.Climate);
+    return(
+    <div className="container">
+    <Barchart></Barchart>
+    <h1>Wind:</h1>
+    <h1>Temperature:</h1>
+    <h1>Humidity:</h1>
+    <h1>Soil:</h1>
     </div>);
   } // end of render
 } // end of class
 
-export default withTracker(() => {
-  return {};
+export default withTracker((props) => {
+  return {
+    Climate: Climate.find({}).fetch()
+  };
 })(App);
--- imports/ui/barchart/barchart.js
+++ imports/ui/barchart/barchart.js
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
-import '/imports/ui/App.css';
+import './barchart.css';
 
 import {Bar} from 'react-chartjs-2';
-
 // App component - represents the whole app
-class App extends Component {
+class Barchart extends React.Component {
+
   constructor(props) {
     super(props);
   }
 
   render() {
-    const chartData = {
+    const data = {
       labels: [],
       datasets: []
     };
-    return(<div className="myChart">
-    <Bar data={data} width={100} height={50} options={{ maintainAspectRatio: false }} />
-    </div>);
+    return(
+      <div className="container">
+        <Bar data={data} width={100} height={50} options={{ maintainAspectRatio: false }} />
+      </div>
+    );
+
   } // end of render
 } // end of class
 
-export default withTracker(() => {
-  return {};
-})(App);
+export default withTracker((props) => {
+  return {
+
+  };
+})(Barchart);
