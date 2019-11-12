4934aca93faa10c4f972f3358ffbef960723281d
Francisco G <fsalvador23@gmail.com>
2019-11-12 17:16:57 +0100

    Fixed case problem

---
+++ imports/ui/VizComponents/Linechart/Linechart.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+
+import { Card, Icon } from 'antd';
+import { Line } from 'react-chartjs-2';
+// App component - represents the whole app
+class Linechart extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: "small",
+      icon: "fullscreen"
+    };
+  }
+
+  changeCardSize() {
+    if(this.state.size == "small" ) {
+      this.setState({ size: "large", icon: "fullscreen-exit"});
+    } else {
+      this.setState({ size: "small", icon: "fullscreen"});
+    }
+    // if(this.state.size == "medium") style = { gridColumn: "span 2", gridRow: "span 1" };
+  }
+
+  render() {
+    const data = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(75,192,192,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+    };
+    let style = { gridColumn: "span 1", gridRow: "span 1" };
+    if(this.state.size == "small") { style = { gridColumn: "span 1", gridRow: "span 1" }; } else { style = { gridColumn: "span 2", gridRow: "span 2" }; }
+    return(
+      <div className="vis-card-container" style={style}>
+        <div className="vis-card-header"><h1>{this.props.title}</h1>{this.extraTools()}</div>
+        <div className="vis-card-content">
+      <Line data={data} />
+      </div>
+      </div>
+    );
+
+  } // end of render
+} // end of class
+
+export default withTracker((props) => {
+  return {
+
+  };
+})(Linechart);
